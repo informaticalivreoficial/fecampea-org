3843d513cce08a5b00e782096d950958db894648
renato-Aspire-E5-574 <remontanari@live.com>
2022-09-16 23:42:58 -0300

    template updates

--- app/Http/Controllers/Web/RssFeedController.php
+++ app/Http/Controllers/Web/RssFeedController.php
@@ -4,9 +4,17 @@ namespace App\Http\Controllers\Web;
 
 use App\Http\Controllers\Controller;
 use App\Models\Post;
+use App\Services\ConfigService;
 
 class RssFeedController extends Controller
 {
+    protected $configService;
+
+    public function __construct(ConfigService $configService)
+    {
+        $this->configService = $configService;
+    }
+
     public function feed()
     {
         $posts = Post::orderBy('created_at', 'DESC')->where('tipo', 'artigo')->postson()->limit(10)->get();
